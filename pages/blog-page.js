fdd07fc7d7d47296e6698be9f9d9c4a5c293ed06
kuyt1992 <kuyt1992@gmail.com>
2021-05-31 00:31:22 +0900

    Layout componentを作成し、共通レイアウトを適用。ブログページ、コンタクトページへの遷移まで。

---
+++ pages/blog-page.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const Blog = () => {
+  return (
+    <Layout title="Blog">
+      Blog Page
+    </Layout>
+  )
+}
+
+export default Blog;
---
+++ pages/contact-page.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const Contact = () => {
+  return (
+    <Layout title={Contact}>
+      Contact Page
+    </Layout>
+  )
+}
+
+export default Contact
